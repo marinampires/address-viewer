8c0fdcae57a0e344b17adece919caad2c96018d4
Marina Melo <marinampires@gmail.com>
2016-03-19 20:16:53 -0300

    Create the address model and migration

---
+++ addresses_viewer/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Address(models.Model):
+    lat = models.DecimalField(max_digits=10, decimal_places=6)
+    lng = models.DecimalField(max_digits=10, decimal_places=6)
+    address_name = models.CharField(max_length=500)
+
