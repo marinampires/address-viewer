5cee92181a465d750614d40bdeaa6d3cf169b281
Marina Melo <marinampires@gmail.com>
2016-03-20 00:16:12 -0300

    Retrivieng data from google fusion

--- addresses_viewer/static/assets/js/map.js
+++ addresses_viewer/static/assets/js/map.js
@@ -1,14 +1,19 @@
 var map;
-
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -22.871638, lng: -43.261632},
+    zoom: 11
   });
 
-  addMarker(myLatLng, "Initial");
+  var layer = new google.maps.FusionTablesLayer({
+    query: {
+      select: 'Location',
+      from: '12a2tPdPOGzY-draRlwRrHtAirL1JC7GUXlqa3e8T'
+    }
+  });
+  layer.setMap(map);
+
 
   var geocoder = new google.maps.Geocoder;
   var infowindow = new google.maps.InfoWindow;
@@ -18,23 +23,22 @@ function initMap() {
       geocodeLatLng(geocoder, map, infowindow, event.latLng);
   }); //end addListener
 
-} 
-
+}
 
 // This function is called when the user clicks the UI button requesting
 // a reverse geocode.
 function geocodeLatLng(geocoder, map, infowindow, latlng) {
   geocoder.geocode({'location': latlng}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        map.setZoom(11);
-        marker = addMarker(latlng, results[1].formatted_address)
-        infowindow.setContent(results[1].formatted_address);
-        infowindow.open(map, marker);
 
-      } else {
-        window.alert('No results found');
-      }
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1] && validate_coordenates(results[1])) {
+          //save place id identifier  
+          console.log(results[1].place_id);
+          map.setZoom(11);
+          marker = addMarker(latlng, results[1].formatted_address)
+          infowindow.setContent(results[1].formatted_address);
+          infowindow.open(map, marker);
+      } 
     } else {
       window.alert('Geocoder failed due to: ' + status);
     }
@@ -50,4 +54,11 @@ function addMarker(latlng, title){
   });
 
   return marker;
+}
+
+function validate_coordenates(results){
+  if (results.types.indexOf("country") > -1)
+    return false;
+
+  return true;
 }
